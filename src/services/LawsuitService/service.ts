c318ab897acfd4fe98e17e006fdc183bae8bd4ab
Bruno Panizzi <brunopanizziq@gmail.com>
2025-01-13 22:55:00 -0300

    abstract fetch logic to util function

--- src/services/LawsuitService/service.ts
+++ src/services/LawsuitService/service.ts
@@ -1,37 +1,13 @@
-import {
-  lawsuitSchema,
-  lawsuitWithClientSchema,
-  lawsuitWithClientListSchema,
-} from "./models";
+import { parsedFetch } from "../parsedFetch";
+
+import { lawsuitWithClientListSchema, fullLawsuit } from "./models";
 
 export class LawsuitService {
   async index() {
-    const response = await fetch("http://localhost:8080/lawsuits");
-
-    if (
-      response.ok &&
-      response.headers.get("Content-Type")?.includes("application/json")
-    ) {
-      const data = await response.json();
-      const lawsuits = lawsuitWithClientListSchema.parse(data);
-      return lawsuits;
-    }
-
-    throw new Error("Failed to fetch lawsuit");
+    return parsedFetch("/lawsuits", lawsuitWithClientListSchema);
   }
 
-  async fetch(cnj: string) {
-    const response = await fetch(`http://localhost:8080/lawsuits/${cnj}`);
-
-    if (
-      response.ok &&
-      response.headers.get("Content-Type")?.includes("application/json")
-    ) {
-      const data = await response.json();
-      const lawsuit = lawsuitWithClientSchema.parse(data);
-      return lawsuit;
-    }
-
-    throw new Error("Failed to fetch lawsuit");
+  async show(cnj: string) {
+    return parsedFetch(`/lawsuits/${cnj}`, fullLawsuit);
   }
 }
